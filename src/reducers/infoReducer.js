6f9f9b1adeba182897006451c743c5c875a69361
Onur T <onurtumer@gmail.com>
2017-01-26 21:10:34 +0300

    Created placeholder 404 page and state.info got data.length

--- src/reducers/infoReducer.js
+++ src/reducers/infoReducer.js
@@ -3,10 +3,16 @@ import initialState from './initialState';
 
 export default function infoReducer(state = initialState.info, action) {
   switch (action.type) {
+    case types.MARK_CARD: {
+      const newInfoObj = state;
+      newInfoObj.currentCard += 1;
+      if (action.mark === 1) newInfoObj.correct = newInfoObj.correct + 1;
+      else if (action.mark === 0) newInfoObj.repeat = newInfoObj.repeat + 1;
+      else if (action.mark === -1) newInfoObj.wrong = newInfoObj.wrong + 1;
+      return Object.assign({}, newInfoObj);
+    }
     case types.GET_DATA_SUCCESS:
-      return state;
-    case types.MARK_CARD:
-      return Object.assign({}, state, { currentCard: state.currentCard + 1});
+      return Object.assign({}, state, { length: action.data.length });
     default:
       return state;
   }
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import HomePage from './components/HomePage';
+import NotFound from './components/NotFound';
 import TrainingPage from './components/training/TrainingPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="/train" component={TrainingPage} />
+    <Route path="*" component={NotFound}/>
   </Route>
 );
