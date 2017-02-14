c24af61ab579610744b5b8764798c33d7935e7ac
Onur T <onurtumer@gmail.com>
2017-02-14 23:12:03 +0300

    changed stats to radial percentage bars

--- src/components/training/TrainingStats.js
+++ src/components/training/TrainingStats.js
@@ -1,13 +1,29 @@
 import React, { PropTypes } from 'react';
+import CircularProgressbar from 'react-circular-progressbar';
 
 const TrainingStats = (props) => {
   const { answers, currentCard: current, length } = props.info;
   const progressWidth = length > 0 ? (current / length) * 100 : 0;
+  const percentages = {
+    correct: calculatePercentage(answers.correct, current) || 0,
+    repeat: calculatePercentage(answers.repeat, current) || 0,
+    wrong: calculatePercentage(answers.wrong, current) || 0
+  };
+  function calculatePercentage(amount, length) {
+    return Math.round((amount / length) * 100)
+  };
+  
   return (
     <div className="training-stats">
-      <span className="stat correct">{answers.correct}</span>
-      <span className="stat repeat">{answers.repeat}</span>
-      <span className="stat wrong">{answers.wrong}</span>
+    <div className="stat correct">
+      <CircularProgressbar percentage={percentages.correct} />
+    </div>
+    <div className="stat repeat">
+      <CircularProgressbar percentage={percentages.repeat} />
+    </div>
+    <div className="stat wrong">
+      <CircularProgressbar percentage={percentages.wrong} />
+    </div>
       <div className="progress progress-bar">
         <div style={{width: `${progressWidth}%`}} />
       </div>
